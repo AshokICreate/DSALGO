a657d831779ae29c9c7926937e2a5186914cb2ac
ashok_r <arooparam@tekion.com>
2019-03-30 12:22:19 +0530

    isSubSequence

---
+++ exercise/multi_pointer_is_subSequence.js
@@ -0,0 +1,18 @@
+function isSubsequence(str1, str2) {
+	if(!str1) {
+	   return true;
+    }
+    let i = 0;
+    let j = 0;
+    while(j<str2.length) {
+            if(str1[i] === str2[j]) {
+                    i++;
+            }
+            if(i === str1.length) {
+                    return true;
+            }
+            j++;
+    }
+}
+
+isSubsequence('sing', 'sting');
