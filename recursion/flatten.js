fc81e9eabd35884f8405b8f6b4002fbaa381890c
ashok_r <arooparam@tekion.com>
2019-03-31 19:12:49 +0530

    flatten array.

---
+++ recursion/flatten.js
@@ -0,0 +1,19 @@
+//write a recursive function called flatten which accepts an arrays and returns a new array with all values flattened.
+
+function flatten(oldArr){
+    // add whatever parameters you deem necessary - good luck!
+     let newArr = [];
+     for(let i = 0; i < oldArr.length; i++) {
+         if(Array.isArray(oldArr[i])) {
+           newArr = newArr.concat(flatten(oldArr[i]));
+         } else {
+             newArr.push(oldArr[i]);
+         }
+     }
+     return newArr;
+  }
+  
+  // flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
+  // flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+  // flatten([[1],[2],[3]]) // [1,2,3]
+  // flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3
